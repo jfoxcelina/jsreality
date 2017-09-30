3368e5caf0af5779f04142334b14c9e22e8c45a0
Jeff Fox <jeff@celinasoftware.com>
2017-09-30 16:56:51 -0500

    added domReady console.log

--- aframe/js/demo1.js
+++ aframe/js/demo1.js
@@ -3,26 +3,37 @@
 var SCENE;
 
 var domReady = function(callback) {
-  document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
+    document.readyState === "interactive" || document.readyState === "complete"
+        ? callback()
+        : document.addEventListener("DOMContentLoaded", callback);
 };
 
 domReady(function() {
-  setup();
-  addCylinder( {x: -1.5, y: 1, z: -3.5}, '#EE335F' );
-  addCylinder( {x: 1.5, y: 1, z: -4}, '#00BBDD' );
+    console.log('domReady');
+    setup();
+    addCylinder({
+        x: -1.5,
+        y: 1,
+        z: -3.5
+    }, '#EE335F');
+    addCylinder({
+        x: 1.5,
+        y: 1,
+        z: -4
+    }, '#00BBDD');
 });
 
 function setup() {
-    SCENE =  document.querySelector('a-scene');
+    SCENE = document.querySelector('a-scene');
 }
 
 function addCylinder(position, color) {
-  var cylinder = document.createElement('a-cylinder');
+    var cylinder = document.createElement('a-cylinder');
 
-  cylinder.setAttribute('color', color);
-  cylinder.setAttribute('height', '2');
-  cylinder.setAttribute('radius', '0.75');
-  cylinder.setAttribute('position', position);
+    cylinder.setAttribute('color', color);
+    cylinder.setAttribute('height', '2');
+    cylinder.setAttribute('radius', '0.75');
+    cylinder.setAttribute('position', position);
 
-  SCENE.appendChild(cylinder);
+    SCENE.appendChild(cylinder);
 }
